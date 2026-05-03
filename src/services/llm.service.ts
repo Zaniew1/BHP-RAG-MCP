837e1cf805b64fa9f18c49ca0e80ea90654ab1e5
Mateusz <m.zaniewski1995@gmail.com>
2026-05-03 19:11:59 +0200

    chore: create LLM class

--- src/services/llm.service.ts
+++ src/services/llm.service.ts
@@ -1,34 +1,32 @@
-// src/services/llm.service.ts
+import { ChatOpenAI } from "@langchain/openai";
+import { LLM_MODEL, OPENAI_API_KEY } from "../utils/constants";
+import { RagInterface } from "./rag.service";
 
-// import { ChatOpenAI } from "@langchain/openai";
-// import { LLM_MODEL } from "../utils/constants";
-// const llm = new ChatOpenAI({
-//   model: LLM_MODEL,
-//   temperature: 0.2,
-// });
+export class LLM{
+    constructor(private rag: RagInterface){}
+    private llm = new ChatOpenAI({
+        model: LLM_MODEL,
+        temperature: 0.2,
+        apiKey: OPENAI_API_KEY,
+    });
 
-// interface GenerateAnswerParams {
-//   question: string;
-//   context: string;
-// }
 
-// export async function generateAnswer({
-//   question,
-//   context,
-// }: GenerateAnswerParams): Promise<string> {
-//   const prompt = `
-// You are a helpful RAG assistant.
-// Answer the user's question ONLY using the provided context.
-// If the answer is not in context, say you don't know.
+public async generateAnswer(prompt:string): Promise<string> {
 
-// Context:
-// ${context}
+    const context = await this.rag.augmentPrompt(prompt)
+    const newPrompt = `
+        You are a helpful RAG assistant.
+        Answer the user's question ONLY using the provided context.
+        If the answer is not in context, say you don't know.
 
-// Question:
-// ${question}
-// `;
+        Context:
+        ${context}
 
-//   const response = await llm.invoke(prompt);
+        Question:
+        ${prompt}
+    `;
+    const response = await this.llm.invoke(newPrompt);
+    return response.content.toString();
+}
 
-//   return response.content.toString();
-// }
+}
